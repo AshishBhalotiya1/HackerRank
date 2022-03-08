1313f1bb65a5cb0d370900452b726bd91d5837b7
AshishBhalotiya1 <97389044+AshishBhalotiya1@users.noreply.github.com>
2022-03-08 18:26:54 +0530

    submit

---
+++ Code 30 days_python3/Day_19 Interfaces.py
@@ -0,0 +1,21 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        factors = []
+        count = 1
+        while count <= n:
+            if n % count == 0:
+                factors.append(count)
+            count += 1
+        return sum(factors)
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
